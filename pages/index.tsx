02f2c5d90fdb27fcea1f76be4d8219f4457f9c17
tushar <tushar158gupta@gmail.com>
2022-11-19 09:59:36 +0530

    parameter destructuring is differnt in tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import {Flex , Box , Text ,Button} from "@chakra-ui/react";
 // import{baseurl , fetchApi} from . 
 
-const Banner=({purpose , title1 , title2 , desc , buttonText , imageurl , linkname })=>(
+const Banner=({purpose , title1 , title2 , desc , buttonText , imageurl , linkname }: { purpose: string; title1: string ; title2:string;desc:string;buttonText:string; imageurl:string ; linkname:string; })=>(
 <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
   <Image src={imageurl} width={500} height={300} alt="banner" />
 <Box p="5">
